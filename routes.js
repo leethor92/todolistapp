4406bb38f1bf580b81210e9a3b8a7f29af30996b
Glitch (todolistapplee) <none>
2018-11-24 23:42:04 +0000

    commit that adds delete functionality

--- routes.js
+++ routes.js
@@ -5,9 +5,12 @@ const router = express.Router();
 
 const dashboard = require('./controllers/dashboard.js');
 const about = require('./controllers/about.js');
+const todolist = require('./controllers/todolist.js');
 
 router.get('/', dashboard.index);
 router.get('/dashboard', dashboard.index);
 router.get('/about', about.index);
+router.get('/todolist/:id', todolist.index);
+router.get('/todolist/:id/deletetask/:taskid', todolist.deleteTask);
 
 module.exports = router;
